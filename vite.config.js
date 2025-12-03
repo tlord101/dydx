856e9159600c6cf5ac71dcaa1aa02f442dc41a1a
David Oweilayefa <kook28515@gmail.com>
2025-12-03 16:49:50 +0100

    Add server proxy configuration to vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,20 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'build'
+  },
+  server: {
+    host: true, // Allows access from mobile (network IP)
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000', // Redirects /api to your local backend
+        changeOrigin: true,
+        secure: false,
+      }
+    }
   }
-});
+})
